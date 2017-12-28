ac91fb2558c982d34bec40741a558ab15f7a3b73
gjjoo <joogeunjae@gmail.com>
2017-12-28 19:52:52 +0900

    set jest, enzyme, storybook

---
+++ src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from './App';
+
+describe('App', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});

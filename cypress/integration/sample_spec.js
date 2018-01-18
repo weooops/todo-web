41c3c42d03301ca6801ffdb8a5dcc97dbf7df6a6
gjjoo <joogeunjae@gmail.com>
2018-01-19 01:44:33 +0900

    all in one 테스트 라이브러리 추가 tsconfig.json 수정 - skipLibCheck: true cypress의 의존모듈중 “@types/mocha”가 “@types/jest”와 변수 선언적 문제에서 충돌이 납니다. 변수 상당수가 똑같은걸 사용하고 있는걸로 파악이 되는데 폴더가 달라도 타입스크립트는 같이 전역으로 바라봅니다. “skipLibCheck” 말 그대로 스킵해주는거 같은데 구글링해서 해결했지만 정확한 용도는 모르겠습니다. 이 설정으로 인해 다른 버그가 생길수도..

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,10 @@
+describe('My First Test', function () {
+  it('test', function () {
+    cy.visit('http://localhost:3000');
+
+    // cy.get('[type="text"]').click();
+
+    // cy.url()
+    //   .should('include', '/search');
+  });
+});
